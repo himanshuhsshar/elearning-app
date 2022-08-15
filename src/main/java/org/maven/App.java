1ca22cfe477631f1f7ac4ca6c07b7ede6fb8d776
himanshuhsshar <himanshu.hs951@gmail.com>
2022-08-15 10:15:43 +0530

    BUG-122 fixed

--- src/main/java/org/maven/App.java
+++ src/main/java/org/maven/App.java
@@ -12,5 +12,9 @@ public class App
         System.out.println( "Hello" );
         System.out.println( "Hey, Ankii!" );
         System.out.println( "Hello" );
+
+        // BUG - 122 fix changes
+        // Logic is completed
+        System.out.println( "Javascript function BUG-122 fixed" );
     }
 }
