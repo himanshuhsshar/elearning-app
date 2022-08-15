b30bc603a8333d7c3a55b072ccee7c0f5b837323
himanshuhsshar <himanshu.hs951@gmail.com>
2022-08-15 10:43:33 +0530

    All Changes Done

--- src/main/java/org/maven/App.java
+++ src/main/java/org/maven/App.java
@@ -13,7 +13,6 @@ public class App
         System.out.println( "Hey, Ankii!" );
         System.out.println( "Hello" );
 
-
         // BUG - 121 fix changes
         System.out.println( "Javascript function BUG-121 fix changes" );
         // Logic is required
